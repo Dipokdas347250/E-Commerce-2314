8111101678feffb46febc3343bed50f516c5cffd
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2024-06-08 16:21:07 +0600

    demo

--- src/components/slice/productSlice.js
+++ src/components/slice/productSlice.js
@@ -19,20 +19,26 @@ export const productSlice = createSlice({
     },
     productIncrement:(state,action) =>{
      state.cartItem[action.payload].qun  += 1
+     localStorage.setItem("cart" , JSON.stringify(state.cartItem))
     },
     productDecrement:(state,action) =>{
       if(state.cartItem[action.payload].qun > 1){
-
         state.cartItem[action.payload].qun  -= 1
+        localStorage.setItem("cart" , JSON.stringify(state.cartItem))
       }else{
 
       }
-    }
+    },
+    removeProduct:(state,action)=>{
+      state.cartItem.splice(action.payload,1)
+      localStorage.setItem("cart" , JSON.stringify(state.cartItem))
+    },
+    
    
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { addTocart ,productIncrement ,productDecrement} = productSlice.actions
+export const { addTocart ,productIncrement ,productDecrement,removeProduct} = productSlice.actions
 
 export default productSlice.reducer
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import Flex from "../components/Flex"
 import { GiTireIronCross } from "react-icons/gi";
 import logo01 from "../assets/Logo.png"
 import { useDispatch, useSelector } from 'react-redux'
-import { productIncrement,productDecrement } from '../components/slice/productSlice';
+import { productIncrement,productDecrement, removeProduct } from '../components/slice/productSlice';
 
 
 const Cart = () => {
@@ -18,9 +18,20 @@ const Cart = () => {
 
   })
    
+ let handleRemove =((index)=>{
+   dispatch(removeProduct(index))
+ })
+
+  const {totalprice, totalquantity} = data.reduce((acc , item)=>{
+    acc.totalprice += item.price * item.qun
+    acc.totalquantity += item.qun
+
+    return acc
 
 
-    
+  },{totalprice:0, totalquantity:0})
+    console.log(totalprice);
+    console.log(totalquantity);
 
 
   return (
@@ -47,7 +58,7 @@ const Cart = () => {
           
           <div className="w-[40%]">
             <div className="flex justify-around items-center">
-              <div className="" >
+              <div className="cursor-pointer" onClick={()=>handleRemove(index)} >
                  <GiTireIronCross/> 
               
               </div>
@@ -71,12 +82,46 @@ const Cart = () => {
               </div>
           </div>
           <div className="w-[15%]">
-          <h4 className='font-sans font-semibold   text-[16px]  text-[#262626] text-center '>${item.price}</h4>
+          <h4 className='font-sans font-semibold   text-[16px]  text-[#262626] text-center '>${item.price * item.qun}</h4>
           </div>
 
         </Flex>
 
         ))}
+
+        <div className="flex justify-end">
+         <div className="">
+         <h2 className='font-sans font-bold   text-[24px]  text-[#262626] text-end '>Cart totals</h2>
+          <div className="flex w-[400px] border-2 border-[#222] justify-around">
+            <div className="">
+              <h3>Subtotal</h3>
+            </div>
+            <div className="">
+              <h4>{totalprice}$</h4>
+            </div>
+          </div>
+          <div className="flex  w-[400px] border-2 border-[#222] justify-around my-3">
+            <div className="">
+            <h3>Quantity</h3>
+            </div>
+            <div className="">
+            <h4>{totalquantity}</h4>
+            </div>
+          </div>
+          <div className="flex  w-[400px] border-2 border-[#222] justify-around">
+            <div className="">
+            <h3>total</h3>
+            </div>
+            <div className="">
+            <h4>{totalprice}$</h4>
+            </div>
+          </div>
+          <div className="mt-2">
+            <p  className=' h-[50px] w-[180px] border-2 border-[#262626] bg-[#000] font-sans font-bold   text-[16px]  text-[#fff] text-center duration-300 ease-in-out leading-[50px] hover:bg-[#fff] hover:text-[#262626] cursor-pointer'>Proceed to Checkout</p>
+          </div>
+         
+         </div>
+        </div>
        </Container>
        
     </section>
